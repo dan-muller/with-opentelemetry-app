6d01e12a8a39e7f5e62a9be3608b1f38ace95658
Daniel Muller <dmuller720@gmail.com>
2024-03-09 21:45:58 -0500

    working!

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,12 @@
 import { registerOTel } from "@vercel/otel";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 
 export function register() {
-  registerOTel("next-app-2");
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    registerOTel({
+      serviceName: "with-opentelemetry-app",
+      spanProcessors: [new SimpleSpanProcessor(new OTLPTraceExporter())],
+    });
+  }
 }
